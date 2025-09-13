2781da76bdb867090b3be6cd137178d5076f820f
Humble <franciscoleman99@gmail.com>
2025-09-13 01:48:21 +0000

    Changed made to encoding for the csv file

--- visualization.py
+++ visualization.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 
 # Note: I used my own dummy data, and this code loads it
-df = pd.read_csv("houses_fire_risk.csv")
+df = pd.read_csv("houses_fire_risk.csv", encoding="latin-1")
 
 # Scatter plot: Fire intensity by location
 plt.figure(figsize=(10, 6))
