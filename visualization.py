190ddc9515399d5371cb69aebffcb90b192c9adf
Humble <franciscoleman99@gmail.com>
2025-09-14 22:58:19 +0000

    Uploaded a new dataset

--- visualization.py
+++ visualization.py
@@ -3,13 +3,15 @@ import matplotlib.pyplot as plt
 
 
 # Note: I used my own dummy data, and this code loads it
-df = pd.read_csv("houses_fire_risk.csv", encoding="latin-1")
+df = pd.read_csv("Houses_dataset.csv", encoding="latin-1")
+print(df.columns)
+
 
 # Scatter plot: Fire intensity by location
 plt.figure(figsize=(10, 6))
 sc = plt.scatter(df['Longitude'], df['Latitude'], 
-                 c=df['Fire_Intensity (%)'], cmap='hot', s=120, edgecolors='black')
-plt.colorbar(sc, label="Fire Intensity (%)")
+                 c=df['Fire Intensity Level'], cmap='hot', s=120, edgecolors='black')
+plt.colorbar(sc, label="Fire Intensity Level")
 plt.title("Visualization of Houses at Risk of Fire by Intensity")
 plt.xlabel("Longitude")
 plt.ylabel("Latitude")
